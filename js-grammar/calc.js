32676153b225c4586e7606808e57a57ed63f94c6
tomitz <tomitz.320@gmail.com>
2020-04-13 21:24:10 +0900

    :sparkles: Finished 1-7

---
+++ js-grammar/calc.js
@@ -0,0 +1,15 @@
+var a = 'JavaScript の変数';
+document.write(a);
+
+var x = 34;
+var y = -0.25 * x + 14.1;
+document.write('<br>' + y)
+
+// このコードは三角形の面積を求めるプログラムです。
+var w = 3; // 三角形の底辺
+var h = 10; // 三角形の高さ
+var area = (w * h) / 2; // 三角形の面積の公式「（底辺）×（高さ）÷2」を使って計算し、areaという名前の変数に代入する
+document.write('<br>三角形の面積 = ' + area); // 面積を出力する
+
+var radius = 12
+document.write('<br>半径 ' + radius + ' cm の円の面積 = ' + (radius * radius * 3.14))
